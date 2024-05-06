86171ba228e3e6dd3e9058c3f4459e52f041d660
wjoates <156824297+wjoates@users.noreply.github.com>
2024-05-06 10:38:18 -0400

    Final Project: edited the home page

--- index.php
+++ index.php
@@ -6,20 +6,16 @@
         <p>Starting out as a local food truck in 1989, Ahli Babas' Kabob Shop has been slinging pitas for 35 years. Shortly after starting the food truck, Ahli Baba's established itself on the vending scene. Selling lot food at Phish and Dead concerts soon turned into a large-scale food operation. Today you can see us at some of the biggest festivals across the east coast, such as Bonnaroo, Electric Forest, and Suwannee Fest. In 1995 our restaurant opened on the corner of Main Street and quickly became one of the go-to late-night spots in town. Ahli Baba's is more then an affordable and delicous sandwhich. Its a staple of Burlington. With the ever changing state of the world and the town of Burlington, rest assured the same old Ahli Babas you love will never change!</p>
     </section>
 
-
     <section id="history-image">
     <figure>
         <img src="../final/images/show.food_.alibaba.jpg" alt="Old Front of Ahli Babas' Kabob Shop">
     </figure>
     </section>
 
-
-
     <section id="popular-dishes">
         <h2>Popular Dishes</h2>
         <p>Ahli Babas has a wide range of dishes meant for everyone. Our big three are definitely the chicken kabob, falafel, and of course the Gyro. What makes Ahli Babas loved is our dedication to preparing every ingredient with the utmost care and refusal to use premade food. Having a successful business in Burlington Vermont also means you have to account for all dietary restrictions. Ahli babas has always had pride in its numerous vegatarian and vegan options along with alergy and gluten replacments. Picky eaters? Get the chicken tender and fry basket! Ahli Babas is a one stop shop for everyone!</p>
     </section>
-
 
     <section id="dishes-image">
         <img src="../final/images/falafel_photo.png" alt="Popular Dishes at Ahli Baba's">
