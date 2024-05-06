7d895016b81c9b8a6be9ad7afb1682c047bdf84d
wjoates <156824297+wjoates@users.noreply.github.com>
2024-05-06 10:39:08 -0400

    Final Project: worked on the contact us page.

---
+++ contact.php
@@ -0,0 +1,36 @@
+<?php include 'top.php'; ?>
+<main class="contact-grid">
+    <!-- Contact Information -->
+    <section class="contact-info">
+        <h2>Contact Us</h2>
+        <p>For all business inquires please do not call our posted phone number! It is important for this number to only be used for ordering. If you would like to request us to vend at an event please email us when, where, and how long the event is. Estimated number of people along with payment information is required as well. For smaller catering orders try to call the restaurant morning prior and morning of to place your order! This can ensure we will have sufficient staffing and ingredients to prepare your order with the utmost care and quality! <br>
+            <strong>Phone Extension:</strong> <br>
+            (802) 862-5752<br>
+            <strong>Email:</strong> <br>
+            ahlibabaskabobshop@gmail.com<br>
+            <strong>Instagram:</strong> <br>
+            <a href="https://www.instagram.com/ahlibabaskabobshop/">Instagram</a><br>
+            <strong>Facebook:</strong> <br>
+            <a href="https://www.facebook.com/p/Ahli-Babas-Kabob-Shop-100063590679564/">Facebook</a><br>
+
+        </p>
+    </section>
+
+    <!-- Contact Us Symbol Image -->
+    <section class="contact-symbol">
+        <img class="contact-images" src="../final/images/contact-image.png" alt="Contact Us Symbol">
+    </section>
+
+    <!-- Location Information -->
+    <section class="location-info">
+        <h2>Where to Find Us</h2>
+        <p>You can find our restaurant at 163 Main St Burlington Vermont. Open from 11AM-11PM on weekdays and 11AM-3AM on the weekends, we are slinging pitas every day! UVM students can find us atop the university green where our food truck is set up from 11AM-3PM Monday through Friday. In addition to our food truck and restaurant, we are at various local and cross-state venues. Our most popular events include Bonnaroo, Peach Festival, Electric Forest, Suwanee Festival, and more! Ahli Baba's also vends as several other conceps so be on the look out! We also have concepts such as Burger Ranch, Ponchos, and Bon Ton Roulet. You'll know its us if you see the distinctive green hats and maroon shirts! As those are our typical employee wear. If you do find us, just know a great meal is coming your way!
+        </p>
+    </section>
+
+    <!-- Image of Stand -->
+    <section class="stand-image">
+        <img class="contact-images" src="../final/images/ahlibabas-shop.jpg" alt="Our Stand">
+    </section>
+</main>
+<?php include 'footer.php'; ?>
